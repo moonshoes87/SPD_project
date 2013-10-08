f921cf86a441394efef47480b444bf2746a345d8
Lori J <mintblue87@gmail.com>
2013-10-08 13:14:27 -0700

    starting unittests

--- spd.py
+++ spd.py
@@ -457,11 +457,12 @@ class PintPars(object):
 
 # K temps: [0.0, 100.0, 150.0, 200.0, 225.0, 250.0, 275.0, 300.0, 325.0, 350.0, 375.0, 400.0, 425.0, 450.0, 475.0, 500.0, 525.0, 550.0]
 # C temps: [273, 373.0, 423.0, 473.0, 498.0, 523.0, 548.0, 573.0, 598.0, 623.0, 648.0, 673.0, 698.0, 723.0, 748.0, 773.0, 798.0, 823.0]
-if True:
+thing = PintPars(gui.Data, '0238x6011044', 473., 623.) 
+def do_stuff():
     import new_lj_thellier_gui_spd as tgs
     gui = tgs.Arai_GUI()
     specimens = gui.Data.keys()
-    thing = PintPars(gui.Data, '0238x6011044', 473., 623.)  # this one only gets incorrect results
+    thing = PintPars(gui.Data, '0238x6011044', 473., 623.) 
     thing.calculate_all_statistics()
     thing1 = PintPars(gui.Data, specimens[3], 523., 773.)
     thing1.calculate_all_statistics()
---
+++ spd_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import unittest
+import spd
+
+
+#class ToRomanBadInput(unittest.TestCase):                            
+#    def testTooLarge(self):                                          
+#        """toRoman should fail with large input"""                   
+#        self.assertRaises(roman.OutOfRangeError, roman.toRoman, 4000)
+
+
+
+class TestSpecimenValues(unittest.TestCase):
+
+    def test_name_length(self):
+        self.assertEqual(len(spd.thing.s), 12) 
+        
+    def test_thing_is_pie(self):
+        self.assertRaises(ValueError, spd.YorkRegression, spd.thing)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
