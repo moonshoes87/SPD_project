83fb863ad0a4d257ab2424282fd928f1f4e360a0
Lori J <mintblue87@gmail.com>
2013-10-18 15:33:58 -0700

    add code and tests for getting orientation tensor and reference vector

--- lib_directional_statistics.py
+++ lib_directional_statistics.py
@@ -1,22 +1,23 @@
 #! /usr/bin/env python
 
 import numpy
+from numpy import linalg
 #import copy
 import spd
 
 
 
-def get_partial_zdata(zdata, t_Arai, tmin, tmax):  # oh, right, change this to use start and end.  
+def get_zdata_segment(zdata, t_Arai, tmin, tmax):  # oh, right, change this to use start and end.  
     lower_bound = t_Arai.index(tmin)
     upper_bound = t_Arai.index(tmax)
-    partial_zdata = zdata[lower_bound:upper_bound + 1] # inclusive of upper bound
-    return partial_zdata
+    zdata_segment = zdata[lower_bound:upper_bound + 1] # inclusive of upper bound
+    return zdata_segment
 
 #ex_zdata = [[1., 1., 1.],[1., 2., 3.], [2., 4., 4.5], [2., 3., 7.5]]
 
-def get_cart_primes_and_means(zdata):
+def get_cart_primes_and_means(zdata_segment):
 # seek simpler syntax
-    zdata = numpy.array(zdata)
+    zdata = numpy.array(zdata_segment)
     X1 = zdata[:,0]
     X2 = zdata[:,1]
     X3 = zdata[:,2]
@@ -26,9 +27,27 @@ def get_cart_primes_and_means(zdata):
     X3_prime = list(X3 - means[2])
     return {'X1_prime': X1_prime, 'X2_prime': X2_prime, 'X3_prime': X3_prime, 'X1': X1, 'X2': X2, 'X3': X3, 'X1_mean': means[0], 'X2_mean': means[1], 'X3_mean': means[2], 'means': means }
 
-def get_orientation_tensor(zdata, means):
-    
+def get_orientation_tensor(X1_p, X2_p, X3_p):
+    X1_p, X2_p, X3_p = numpy.array(X1_p), numpy.array(X2_p), numpy.array(X3_p)
+    orient_tensor = [[sum(X1_p * X1_p), sum(X1_p * X2_p), sum(X1_p * X3_p)],
+                     [sum(X1_p * X2_p), sum(X2_p * X2_p), sum(X2_p * X3_p)],
+                     [sum(X1_p * X3_p), sum(X2_p * X3_p), sum(X3_p * X3_p)]]
+    tau, V = numpy.linalg.eig(orient_tensor)
+    return {'orient_tensor': orient_tensor, 'tau': tau, 'V': V}
+
+def get_eigenvectors_and_eigenvalues(orient_tensor):
+    # may need to sort eigenvectors, eigenvalues.  possibly not in order...?
     pass
+
+def get_reference_vector(X1_prime, X2_prime, X3_prime):
+    n = len(X1_prime) - 1
+    X1 = X1_prime[0] - X1_prime[n]
+    X2 = X2_prime[0] - X2_prime[n]
+    X3 = X3_prime[0] - X3_prime[n]
+    print numpy.array([X1, X2, X3])
+    return numpy.array([X1, X2, X3])
+#    return numpy.array([0, 0, 0])
+    
 
 #M = (zdata_segment-mean(zdata_segment.T,axis=1)).T
 
--- test_directional_statistics.py
+++ test_directional_statistics.py
@@ -13,7 +13,8 @@ thing = spd.thing
 class CheckDecInc(unittest.TestCase):
     
     zdata = [[0., 0., 0.],[1., 1., 1.],[1., 2., 3.], [2., 4., 4.5], [2., 3., 7.5], [0., 0., 0.]]
-    partial_zdata = [[1., 1., 1.],[1., 2., 3.], [2., 4., 4.5], [2., 3., 7.5]]
+    zdata_segment = [[1., 1., 1.],[1., 2., 3.], [2., 4., 4.5], [2., 3., 7.5]]
+
     t_Arai = [1.,1.5, 3, 8, 10, 11]
     tmin = 1.5
     tmax = 10.
@@ -30,35 +31,56 @@ class CheckDecInc(unittest.TestCase):
     X2 = [ 1.,  2.,  4.,  3.] 
     X3 = [ 1.,   3.,   4.5,  7.5]
 
-
     X1_p = numpy.array(X1_prime)
     X2_p = numpy.array(X2_prime)
     X3_p = numpy.array(X3_prime)
     # probably can write this more succintly, but good visualization
-    orient_tensor = [[sum(X1_p * X1_p), sum(X1_p * X2_p), sum(X1_p * X3_p)],
-                     [sum(X1_p * X2_p), sum(X2_p * X2_p), sum(X2_p * X3_p)],
-                     [sum(X1_p * X3_p), sum(X2_p * X3_p), sum(X3_p * X3_p)]]
+
+    orientation_tensor = lib_direct.get_orientation_tensor(X1_prime, X2_prime, X3_prime)
+
+    reference_vector = (-1., -2., -6.5)
+    
             
 
-    def test_partial_zdata(self):
-        result = lib_direct.get_partial_zdata(self.zdata, self.t_Arai, self.tmin, self.tmax)
-        self.assertEqual(result, self.partial_zdata)
+
+    def test_zdata_segment(self):
+        result = lib_direct.get_zdata_segment(self.zdata, self.t_Arai, self.tmin, self.tmax)
+        self.assertEqual(result, self.zdata_segment)
 
     def test_cart_averages(self):
-        result = lib_direct.get_cart_primes_and_means(self.partial_zdata)
+        result = lib_direct.get_cart_primes_and_means(self.zdata_segment)
         self.assertEqual(result['X1_mean'], self.X1_avg)
         self.assertEqual(result['X2_mean'], self.X2_avg)
         self.assertEqual(result['X3_mean'], self.X3_avg)
 
     def test_cart_primes(self):
-        result = lib_direct.get_cart_primes_and_means(self.partial_zdata)
+        result = lib_direct.get_cart_primes_and_means(self.zdata_segment)
         self.assertEqual(result['X1_prime'], self.X1_prime)
         self.assertEqual(result['X2_prime'], self.X2_prime)
         self.assertEqual(result['X3_prime'], self.X3_prime)
 
     def test_orientation_tensor(self):
-        pass
-       # result = lib_direct.get_orientation_tensor(self.X1_prime, self.X2_prime, self.X3_prime)
+        X1_prime = [1., 2.]
+        X2_prime = [1., 1.]
+        X3_prime = [-1., -.5]
+        orient_tensor = numpy.array([[5., 3., -2.],
+                                     [3., 2., -1.5],
+                                     [-2., -1.5, 1.25]])
+        orient_tensor = orient_tensor
+        result = lib_direct.get_orientation_tensor(X1_prime, X2_prime, X3_prime)
+        print "result", result
+        print "orient_tensor", orient_tensor
+        v = numpy.allclose(result['orient_tensor'], orient_tensor) # assesses two arrays for if they are approximately eq
+        self.assertTrue(v)
+
+    def test_reference_vector(self):
+        print "reference:", self.reference_vector
+        result = lib_direct.get_reference_vector(self.X1_prime, self.X2_prime, self.X3_prime)
+        print "result", result
+        for num, value in enumerate(result):
+            self.assertAlmostEqual(value, self.reference_vector[num])
+        
+
         
         
 
