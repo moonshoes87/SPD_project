2814f6d7bbfbc3a8a9b7b753614ab216e83eb0b8
Lori J <mintblue87@gmail.com>
2013-10-21 16:56:01 -0700

    ok, eigenvectors are hard

--- lib_directional_statistics.py
+++ lib_directional_statistics.py
@@ -14,23 +14,25 @@ def tauV(T):
     """
     t,V,tr=[],[],0.
     ind1,ind2,ind3=0,1,2
-    # V, tau
+    # tau, V
     evalues,evectmps=numpy.linalg.eig(T)
     print "original V, tau from linalg: ", evalues, evectmps
     evectors=numpy.transpose(evectmps)  # to make compatible with Numeric convention
+    # uncertain about this:
     for tau in evalues:
         tr+=tau  # tr totals tau values
     if tr!=0:
         for i in range(3):
             print "original:", evalues[i]
-            evalues[i]=evalues[i]/tr  # this is some form of norming.  but why?
+            evalues[i]=evalues[i]/tr  # convention is norming eigenvalues so they sum to 1.  why?  just because.  :)
             print "normed:", evalues[i]
     else:
         return t,V  # if eigenvalues add up to zero, no sorting is needed
+    # ending here
 
 # sort evalues,evectors                                            
     t1,t2,t3=0.,0.,1.
-    for k in range(3):
+    for k in range(3):  
         if evalues[k] > t1:
             t1,ind1=evalues[k],k
         if evalues[k] < t3:
@@ -123,10 +125,74 @@ def get_reference_vector(X1_prime, X2_prime, X3_prime):
     X1 = X1_prime[0] - X1_prime[n]
     X2 = X2_prime[0] - X2_prime[n]
     X3 = X3_prime[0] - X3_prime[n]
-    print numpy.array([X1, X2, X3])
-    return numpy.array([X1, X2, X3])
+    R= numpy.array([X1, X2, X3])
+    return R
 #    return numpy.array([0, 0, 0])
     
 
+
+
+
 #M = (zdata_segment-mean(zdata_segment.T,axis=1)).T
 
+ex_zdata = [[1., 1., 1.],[1., 2., 3.], [2., 4., 4.5], [2., 3., 7.5]]
+t = get_cart_primes_and_means(ex_zdata)
+T = get_orientation_tensor(t['X1_prime'], t['X2_prime'], t['X3_prime'])
+tau, V = tauV(T['orient_tensor'])
+V1 = V[0]
+R = get_reference_vector(t['X1_prime'], t['X2_prime'], t['X3_prime'])
+dot = numpy.dot(V1, R) # dot product of reference vector and the principal axis of the V matrix
+if dot < -1:
+    dot = -1
+elif dot > 1:
+    dot = 1
+
+
+PD = []
+if numpy.arccos(dot) > numpy.pi / 2:
+    PD = -V1
+else:
+    PD = V1
+
+
+if PD[0] < 0:
+    Dec = numpy.arctan(PD[1]/PD[0]) + 180.
+elif PD[0] > 0 and PD[0] <= 0
+
+
+
+
+# maybe use this
+def cart2dir(cart):
+    """
+    converts a direction to cartesian coordinates
+    """
+    cart=numpy.array(cart)
+    rad=numpy.pi/180. # constant to convert degrees to radians
+    if len(cart.shape)>1:
+        Xs,Ys,Zs=cart[:,0],cart[:,1],cart[:,2]
+    else: #single vector
+        Xs,Ys,Zs=cart[0],cart[1],cart[2]
+    Rs=numpy.sqrt(Xs**2+Ys**2+Zs**2) # calculate resultant vector length                 
+    Decs=(numpy.arctan2(Ys,Xs)/rad)%360. # calculate declination taking care of correct quadrants (arctan2) and making modulo 360.
+    try:
+        Incs=numpy.arcsin(Zs/Rs)/rad # calculate inclination (converting to degrees) #                             
+    except:
+        print 'trouble in cart2dir' # most likely division by zero somewhere
+        return numpy.zeros(3)
+    return numpy.array([Decs,Incs,Rs]).transpose() # return the directions list
+
+# Lisa sequence:
+#    T=numpy.array(Tmatrix(X))
+#    t,V=tauV(T)
+#    w1,w2,w3=t[2],t[1],t[0]
+#    k1,k2=binglookup(w1,w2)
+#    PDir=cart2dir(V[0])
+#    EDir=cart2dir(V[1])
+#    ZDir=cart2dir(V[2])
+#    if PDir[1] < 0:
+#        PDir[0]+=180.
+#        PDir[1]=-PDir[1]
+#    PDir[0]=PDir[0]%360.
+#    bpars["dec"]=PDir[0]
+#    bpars["inc"]=PDir[1]
